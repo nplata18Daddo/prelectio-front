534549a84053d0e9a9ca996e91fa1f4b75d7eb6e
nplata18Daddo <na.plata@daddosoftware.onmicrosoft.com>
2023-03-23 20:58:28 -0500

    feat:changepass

--- src/pages/auth/forgotPassword.jsx
+++ src/pages/auth/forgotPassword.jsx
@@ -5,6 +5,7 @@ import LogoPrelectio from "../../assets/logo_prelectio.png";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { Link, Routes, Route, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 export const ForgotPassword = () => {
   const schema = yup.object().shape({
     email: yup
@@ -34,89 +35,97 @@ export const ForgotPassword = () => {
   };
   const navigate = useNavigate();
   return (
-    <Row className="forgotPass__background justify-content-center">
-      <Col xs={10} md={6} className=" my-auto">
-        <Card body className="forgotPass__card ">
-          <Row>
-            <Col xs={12} className="px-4">
-              <h1
-                onClick={() => navigate(-1)}
-                className="text-white display__label weight__bold pb-1 d-flex align-items-center"
-                style={{ textAlign: "start", cursor: "pointer" }}
-              >
-                <i
-                  class="bi bi-arrow-left-circle colors__lightBlue"
-                  style={{ fontSize: "20px", marginRight: "15px" }}
-                ></i>
-                <span>Ir atrás</span>
-              </h1>
-            </Col>
-            <Col xs={12} md={8} style={{ padding: "5rem" }}>
-              <Row>
-                <Col xs={12}>
-                  <h1
-                    className="text-white display__large weight__bold"
-                    style={{ textAlign: "start" }}
-                  >
-                    Restablecer Contraseña
-                  </h1>
-                </Col>
-                <Col xs={12}>
-                  <Form onSubmit={handleSubmit(handleForgotPass)}>
-                    <Form.Group
-                      className="mb-5 mt-5"
-                      controlId="formBasicEmail"
+    <motion.main
+      className="main__container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.7 }}
+    >
+      <Row className="forgotPass__background justify-content-center">
+        <Col xs={10} md={6} className=" my-auto">
+          <Card body className="forgotPass__card ">
+            <Row>
+              <Col xs={12} className="px-4">
+                <h1
+                  onClick={() => navigate(-1)}
+                  className="text-white display__label weight__bold pb-1 d-flex align-items-center"
+                  style={{ textAlign: "start", cursor: "pointer" }}
+                >
+                  <i
+                    class="bi bi-arrow-left-circle colors__lightBlue"
+                    style={{ fontSize: "20px", marginRight: "15px" }}
+                  ></i>
+                  <span>Ir atrás</span>
+                </h1>
+              </Col>
+              <Col xs={12} md={8} style={{ padding: "5rem" }}>
+                <Row>
+                  <Col xs={12}>
+                    <h1
+                      className="text-white display__large weight__bold"
                       style={{ textAlign: "start" }}
                     >
-                      <Form.Label className="text-white display__small mb-4">
-                        Correo
-                      </Form.Label>
-                      <InputGroup>
-                        <InputGroup.Text className="login__input__icon display__small">
-                          <i className="bi bi-envelope"></i>
-                        </InputGroup.Text>
-                        <Form.Control
-                          {...register("email")}
-                          className="login__input  display__small"
-                          placeholder="Tu correo"
-                        />
-                        {errors.email && (
-                          <Form.Control.Feedback
-                            className="d-block display__label mt-2"
-                            type="invalid"
-                          >
-                            {errors.email.message}
-                          </Form.Control.Feedback>
-                        )}
-                      </InputGroup>
-                    </Form.Group>
+                      Restablecer Contraseña
+                    </h1>
+                  </Col>
+                  <Col xs={12}>
+                    <Form onSubmit={handleSubmit(handleForgotPass)}>
+                      <Form.Group
+                        className="mb-5 mt-5"
+                        controlId="formBasicEmail"
+                        style={{ textAlign: "start" }}
+                      >
+                        <Form.Label className="text-white display__small mb-4">
+                          Correo
+                        </Form.Label>
+                        <InputGroup>
+                          <InputGroup.Text className="login__input__icon display__small">
+                            <i className="bi bi-envelope"></i>
+                          </InputGroup.Text>
+                          <Form.Control
+                            {...register("email")}
+                            className="login__input  display__small"
+                            placeholder="Tu correo"
+                          />
+                          {errors.email && (
+                            <Form.Control.Feedback
+                              className="d-block display__label mt-2"
+                              type="invalid"
+                            >
+                              {errors.email.message}
+                            </Form.Control.Feedback>
+                          )}
+                        </InputGroup>
+                      </Form.Group>
 
-                    <Button
-                      disabled={!watch("email")}
-                      variant="primary"
-                      type="submit"
-                      className="login__submit display__small weight__bold"
-                    >
-                      Continuar
-                    </Button>
-                  </Form>
-                </Col>
-              </Row>
-            </Col>
-            <Col
-              xs={12}
-              md={4}
-              className="p-5 d-flex justify-content-center align-items-center"
-            >
-              <img
-                className="login__logo"
-                src={LogoPrelectio}
-                alt="logo prelectio"
-              ></img>
-            </Col>
-          </Row>
-        </Card>
-      </Col>
-    </Row>
+                      <Button
+                        disabled={!watch("email")}
+                        variant="primary"
+                        type="submit"
+                        className="login__submit display__small weight__bold"
+                      >
+                        Continuar
+                      </Button>
+                    </Form>
+                  </Col>
+                </Row>
+              </Col>
+              <Col
+                xs={12}
+                md={4}
+                className="p-5 d-flex justify-content-center align-items-center"
+              >
+                <img
+                  className="login__logo"
+                  src={LogoPrelectio}
+                  alt="logo prelectio"
+                ></img>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </motion.main>
   );
 };
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,18 +1,23 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 import { Layout } from "../components/layout";
 import { Home } from "../pages";
 import { ForgotPassword } from "../pages/auth/forgotPassword";
 import { Login } from "../pages/auth/login";
-
+import { AnimatePresence } from "framer-motion";
+import { ChangePass } from "../pages/auth/changePass";
 function AppRoutes() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgotPassword" element={<ForgotPassword />} />
-    </Routes>
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/changePassword" element={<ChangePass />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 export default AppRoutes;
